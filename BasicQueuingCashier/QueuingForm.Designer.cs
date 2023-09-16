51dbd1f8775b8bc7d8cbeecfe5139bd7e2e875a5
Guze <Guze@LENOVO-PC>
2023-09-16 17:30:53 +0800

    final

--- BasicQueuingCashier/QueuingForm.Designer.cs
+++ BasicQueuingCashier/QueuingForm.Designer.cs
@@ -63,7 +63,7 @@
             // 
             this.lblQueue.AutoSize = true;
             this.lblQueue.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblQueue.Location = new System.Drawing.Point(295, 151);
+            this.lblQueue.Location = new System.Drawing.Point(297, 151);
             this.lblQueue.Name = "lblQueue";
             this.lblQueue.Size = new System.Drawing.Size(65, 42);
             this.lblQueue.TabIndex = 2;
